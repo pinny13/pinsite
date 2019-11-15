7e16fb09435fe8af373b68c756cf3f117eadddac
pinny13 <pavel.khey@gmail.com>
2019-11-14 21:53:18 -0800

    Final date picker

--- src/playground/datePicker/index.js
+++ src/playground/datePicker/index.js
@@ -1,6 +1,7 @@
 (() => {
   // Elements and attributes
   const eContainer = document.querySelector(".container");
+  const eControls = document.querySelector(".controls");
   const eDays = document.querySelector(".days");
   const eYearMonth = document.querySelector(".yearMonth");
 
@@ -8,60 +9,140 @@
   const attrCurrentMonth = "data-month";
   const attrCurrentDate = "data-date";
 
-  const prevYear = () => {
-    const year = +eContainer.getAttribute(attrCurrentYear) - 1;
+  const storeDisplayDate = (year, month) => {
+    eControls.setAttribute(attrCurrentYear, year);
+    eControls.setAttribute(attrCurrentMonth, month);
+  };
+
+  const getDisplayDate = () => {
+    return {
+      year: eControls.getAttribute(attrCurrentYear),
+      month: eControls.getAttribute(attrCurrentMonth)
+    };
+  };
+
+  const storeSelectedDate = (year, month, date) => {
     eContainer.setAttribute(attrCurrentYear, year);
-    const month = +eContainer.getAttribute(attrCurrentMonth);
+    eContainer.setAttribute(attrCurrentMonth, month);
+    eContainer.setAttribute(attrCurrentDate, date);
+  };
+
+  const getSelectedDate = () => {
+    return {
+      year: eContainer.getAttribute(attrCurrentYear),
+      month: eContainer.getAttribute(attrCurrentMonth),
+      date: eContainer.getAttribute(attrCurrentDate)
+    };
+  };
+
+  const prevYear = () => {
+    let { year, month } = getDisplayDate();
+    year--;
     eYearMonth.innerHTML = setupDayDivs(year, month);
-    displayDate();
+    storeDisplayDate(year, month);
+    displayYearMonth();
     setupDayDivs();
   };
   const prevMonth = () => {
-    let year = +eContainer.getAttribute(attrCurrentYear);
-    let month = +eContainer.getAttribute(attrCurrentMonth) - 1;
+    let { year, month } = getDisplayDate();
+    month--;
     if (month < 0) {
       month = 11;
       year--;
     }
-    eContainer.setAttribute(attrCurrentYear, year);
-    eContainer.setAttribute(attrCurrentMonth, month);
-    displayDate();
+    storeDisplayDate(year, month);
+    displayYearMonth();
     setupDayDivs();
   };
   const nextYear = () => {
-    const year = +eContainer.getAttribute(attrCurrentYear) + 1;
-    eContainer.setAttribute(attrCurrentYear, year);
-    const month = +eContainer.getAttribute(attrCurrentMonth);
-    displayDate();
+    let { year, month } = getDisplayDate();
+    year++;
+    storeDisplayDate(year, month);
+    displayYearMonth();
     setupDayDivs();
   };
   const nextMonth = () => {
-    let year = +eContainer.getAttribute(attrCurrentYear);
-    let month = +eContainer.getAttribute(attrCurrentMonth) + 1;
+    let { year, month } = getDisplayDate();
+    month++;
     if (month > 11) {
       month = 0;
       year++;
     }
-    eContainer.setAttribute(attrCurrentYear, year);
-    eContainer.setAttribute(attrCurrentMonth, month);
-    displayDate();
+    storeDisplayDate(year, month);
+    displayYearMonth();
     setupDayDivs();
   };
 
-  const displayDate = () => {
-    const year = +eContainer.getAttribute(attrCurrentYear);
-    const month = +eContainer.getAttribute(attrCurrentMonth) + 1;
+  const displayYearMonth = () => {
+    let { year, month } = getDisplayDate();
+    month++;
     eYearMonth.innerHTML = `${year} / ${month}`;
   };
 
-  const setupDayDivs = () => {
-    const year = +eContainer.getAttribute(attrCurrentYear);
-    const month = +eContainer.getAttribute(attrCurrentMonth);
-
-    // TODO: IMPLEMENT ME!
-    console.log(`Populate day divs for: ${year} / ${month}`);
+  const getNumOfMonthDays = date => {
+    let result = 0;
+    const month = date.getMonth();
+    let currentMonth = month;
+    while (currentMonth === month) {
+      let currDate = date.getDate();
+      date.setDate(currDate + 1);
+      currentMonth = date.getMonth();
+      result++;
+    }
+    return result;
   };
-  const displaySelectedDate = () => {};
+
+  const setupDayDivs = () => {
+    //Clear previous days
+    eDays.querySelectorAll("div").forEach(element => {
+      element.innerHTML = "";
+      element.className = "";
+    });
+
+    let { year, month } = getDisplayDate();
+    let selectedYear = getSelectedDate().year;
+    let selectedMonth = getSelectedDate().month;
+    let selectedDate = getSelectedDate().date;
+
+    const date = new Date();
+    date.setFullYear(year);
+    date.setMonth(month);
+    date.setDate(1);
+    const startDay = date.getDay();
+    const numOfMonthDays = getNumOfMonthDays(date);
+
+    for (let i = startDay, d = 1; d <= numOfMonthDays; i++, d++) {
+      const eDay = eDays.children[i];
+      eDay.innerHTML = d;
+      eDay.addEventListener("click", () => {
+        storeSelectedDate(year, month, d);
+
+        displaySelectedDate();
+        eDays
+          .querySelectorAll("div")
+          .forEach(element => (element.className = ""));
+
+        eDay.className = "selected";
+      });
+
+      if (
+        year === selectedYear &&
+        month === selectedMonth &&
+        d === +selectedDate
+      ) {
+        eDay.className = "selected";
+      }
+    }
+  };
+  const displaySelectedDate = () => {
+    const year = +eContainer.getAttribute(attrCurrentYear);
+    const month = +eContainer.getAttribute(attrCurrentMonth) + 1;
+    const day = +eContainer.getAttribute(attrCurrentDate);
+
+    document.querySelector(
+      "#currentDate"
+    ).innerHTML = `${month}/${day}/${year}`;
+  };
 
   const addListener = (selector, listener) => {
     const elem = document.querySelector(selector);
@@ -70,16 +151,15 @@
 
   const init = () => {
     // Create 35 divs for days
-    for (let i = 0; i < 35; i++) {
+    for (let i = 0; i < 42; i++) {
       const eDay = document.createElement("div");
       eDays.appendChild(eDay);
     }
 
     // Set current date as the date of the picker
     const now = new Date();
-    eContainer.setAttribute(attrCurrentYear, now.getFullYear());
-    eContainer.setAttribute(attrCurrentMonth, now.getMonth());
-    displayDate();
+    storeDisplayDate(now.getFullYear(), now.getMonth());
+    displayYearMonth();
 
     //Populate 35 divs with date as of today's month and attach event handlers
     setupDayDivs();
