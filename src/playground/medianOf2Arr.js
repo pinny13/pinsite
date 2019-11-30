76c87e031161f439cd62e0f57b5eb0a492930786
pinny13 <pavel.khey@gmail.com>
2019-11-30 15:34:07 -0800

    Updates

--- src/playground/medianOf2Arr.js
+++ src/playground/medianOf2Arr.js
@@ -26,32 +26,49 @@ The median is (2 + 3)/2 = 2.5
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function (nums1, nums2) {
-
-    let newArr = [];
-    if (!nums1 || !nums1.length) {
-        newArr = nums2;
-    } else if (!nums2 || !nums2.length) {
-        newArr = nums1;
+var findMedianSortedArrays = function(nums1, nums2) {
+  const getMedian = arr => {
+    if (arr.length % 2 !== 0) {
+      return arr[Math.floor(arr.length / 2)];
     } else {
-        newArr = nums1.concat(nums2).sort();
+      const mid = arr.length / 2;
+      const a = arr[mid - 1];
+      const b = arr[mid];
+      return (a + b) / 2;
     }
+  };
 
-    // or 
-    let n1 = Array.from(nums1);
-    let n2 = Array.from(nums2);
-    
+  const merge1 = () => {
+    return nums1.concat(nums2).sort((a, b) => a - b);
+  };
 
+  let newArr = [];
 
-    if (newArr.length % 2 !== 0) {
-        return newArr[Math.floor(newArr.length / 2)];
+  if (!nums1 || !nums1.length) {
+    newArr = nums2;
+  } else if (!nums2 || !nums2.length) {
+    newArr = nums1;
+  } else {
+    if (nums1.length === 1 && nums2.length === 1) {
+      const a = Math.min(nums1[0], nums2[0]);
+      const b = Math.max(nums1[0], nums2[0]);
+      return (a + b) / 2;
     } else {
-        const mid = newArr.length / 2;
-        const a = newArr[mid - 1];
-        const b = newArr[mid];
-        return (a + b) / 2;
+      newArr = merge1();
     }
+  }
+
+  return getMedian(newArr);
 };
 
-document.querySelector('#m2a_1').appendChild(document.createTextNode(findMedianSortedArrays([1, 3], [2])));
-document.querySelector('#m2a_2').appendChild(document.createTextNode(findMedianSortedArrays([1, 2], [3, 4])));
+/*
+document
+  .querySelector("#m2a_1")
+  .appendChild(document.createTextNode(findMedianSortedArrays([1, 3], [2])));
+document
+  .querySelector("#m2a_2")
+  .appendChild(document.createTextNode(findMedianSortedArrays([1, 2], [3, 4])));
+*/
+document
+  .querySelector("#m2a_3")
+  .appendChild(document.createTextNode(findMedianSortedArrays([3], [-2, -1])));
