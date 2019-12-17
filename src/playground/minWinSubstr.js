057e245b08499ffe02a022bbd715d41c52970a47
pinny13 <pavel.khey@gmail.com>
2019-12-16 21:18:50 -0800

    updates

--- src/playground/minWinSubstr.js
+++ src/playground/minWinSubstr.js
@@ -19,9 +19,61 @@ If there is such window, you are guaranteed that there will always be only one u
  * @return {string}
  */
 var minWindow = function(s, t) {
-  return "XYZ";
+  if (!s || !t) {
+    return "";
+  }
+
+  if (t.length > s.length) {
+    return "";
+  }
+
+  //Init
+  let remainingLetters = Array.from(t);
+  let start = -1;
+  let mapOfPos = new Map();
+  let foundChars = [];
+  let currChar = "";
+  let result = "";
+
+  for (let i = 0; i < s.length; i++) {
+    currChar = s[i];
+
+    if (t.includes(currChar)) {
+      if (start === -1) {
+        start = i;
+      }
+
+      if (remainingLetters.includes(currChar)) {
+        // This is the first time we see this char
+        remainingLetters.splice(remainingLetters.indexOf(currChar), 1);
+        foundChars.push(currChar);
+      } else {
+        // This is NOT the first time we see this char
+        foundChars.splice(foundChars.indexOf(currChar), 1);
+        foundChars.push(currChar);
+
+        if (start === mapOfPos.get(currChar)) {
+          //update start to next closest pos of found char
+          // start = Math.max(i, mapOfPos.get(foundChars[0]));
+          start = mapOfPos.get(foundChars[0]);
+        }
+      }
+
+      if (!remainingLetters.length) {
+        let tempResult = s.substring(start, i + 1);
+
+        if (!result.length || tempResult.length < result.length) {
+          result = tempResult;
+        }
+      }
+
+      mapOfPos.set(currChar, i);
+    }
+  }
+
+  return result;
 };
 
-document.querySelector('#mws_1').innerHTML = minWindow("", "");
-document.querySelector('#mws_2').innerHTML = minWindow("", "");
-document.querySelector('#mws_3').innerHTML = minWindow("", "");
+// document.querySelector("#mws_1").innerHTML = minWindow("ADOBECODEBANC", "ABC");
+// document.querySelector("#mws_2").innerHTML = minWindow("AXBCXXA", "ABC");
+document.querySelector("#mws_3").innerHTML = minWindow("bba", "ab");
