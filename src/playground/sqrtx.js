bc8bc104cbb40657635b613a66d1efddc300b148
pinny13 <pavel.khey@gmail.com>
2020-01-11 11:20:19 -0800

    adding sqrtx to playground

---
+++ src/playground/sqrtx.js
@@ -0,0 +1,46 @@
+/*
+https://leetcode.com/problems/sqrtx/
+
+
+Implement int sqrt(int x).
+
+Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+
+Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+
+Example 1:
+
+Input: 4
+Output: 2
+Example 2:
+
+Input: 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since 
+             the decimal part is truncated, 2 is returned.
+*/
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+  if (x === 0) {
+    return 0;
+  }
+  if (x < 4) {
+    return 1;
+  }
+
+  let result = 2;
+
+  while (result * result <= x) {
+    result += 1;
+  }
+
+  return result - 1;
+};
+
+document.querySelector("#sqrt_1").innerHTML = mySqrt(4);
+document.querySelector("#sqrt_2").innerHTML = mySqrt(8);
+document.querySelector("#sqrt_3").innerHTML = mySqrt(9);
