228a3e726b769507dfc466c18708c0465bb95602
pinny13 <pavel.khey@gmail.com>
2020-08-08 18:01:40 -0700

    adding ability to change theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import logo from "./logo.svg";
 import "./App.css";
 import { NavBar } from "./navigation";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { HomePage } from "./home";
 import { PlaygroundPage } from "./playground";
 import { ResourcesPage } from "./resources";
--- src/navigation/navBar.tsx
+++ src/navigation/navBar.tsx
@@ -2,38 +2,55 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./navBar.scss";
 
-export class NavBar extends React.Component {
-  public render() {
-    return (
-      <nav id="nav-bar">
-        <ul>
-          <li>
-            <NavLink to="/" activeClassName="selected" exact>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/playground" activeClassName="selected">
-              Playground
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/resources" activeClassName="selected">
-              Resources
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/about" activeClassName="selected">
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact" activeClassName="selected">
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    );
+function toggleTheme() {
+  const htmlTag = document.getElementsByTagName("html")[0];
+  const toggler = document.querySelector("#themeToggler");
+  if (htmlTag.hasAttribute("data-theme")) {
+    htmlTag.removeAttribute("data-theme");
+    toggler!.innerHTML = "In dark";
+    return;
   }
+
+  htmlTag.setAttribute("data-theme", "dark");
+  toggler!.innerHTML = "In light";
 }
+
+const NavBar = () => {
+  return (
+    <nav id="nav-bar">
+      <ul>
+        <li>
+          <NavLink to="/" activeClassName="selected" exact>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/playground" activeClassName="selected">
+            Playground
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/resources" activeClassName="selected">
+            Resources
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" activeClassName="selected">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" activeClassName="selected">
+            Contact
+          </NavLink>
+        </li>
+
+      [<span id="themeToggler" className="theme-toggle" onClick={toggleTheme}>
+        In dark
+      </span>]
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
