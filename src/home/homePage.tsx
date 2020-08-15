f575e6f9cf86842e6bab332ca91cb1ff55573597
pinny13 <pavel.khey@gmail.com>
2020-08-14 22:30:06 -0700

    updates

--- src/home/homePage.tsx
+++ src/home/homePage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>This is home page!</div>;
+  return (
+    <div>
+      <img src="me.jpg" />
+      <div>
+        Hi! I ma Pavel, Front End Engineer and "everything web" fan from Seattle
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
